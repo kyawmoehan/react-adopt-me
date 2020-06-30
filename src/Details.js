74c6a8bd6a5f9a07e42738df03dd572c428f8cd7
kyawmoehan <kyawmoehan012@gmail.com>
2020-06-30 20:07:47 +0630

    Add animal carousel image show

--- src/Details.js
+++ src/Details.js
@@ -1,13 +1,11 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
 
 class Details extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true,
-    };
-  }
+  state = {
+    loading: true,
+  };
   componentDidMount() {
     pet.animal(this.props.id).then(({ animal }) => {
       this.setState({
@@ -26,9 +24,10 @@ class Details extends React.Component {
     if (this.state.loading) {
       return <h1 className="loading">Loading ...</h1>;
     }
-    const { animal, breed, location, description, name } = this.state;
+    const { animal, breed, location, description, name, media } = this.state;
     return (
       <div className="details">
+        <Carousel media={media} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
